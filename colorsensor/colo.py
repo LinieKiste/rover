d9bae2c17eec3136e739d94d924447a271c2f850
Leander <leander.buerkin@gmail.com>
2021-10-18 17:00:12 +0000

    moved color detection into one function

--- colorsensor/colo.py
+++ colorsensor/colo.py
@@ -7,62 +7,70 @@ import time
 
 class colorSens:
 
+    color = None
+    color_rgb = None
+
     def __init__(self):
         self.i2c = board.I2C()
         self.sensor = adafruit_tcs34725.TCS34725(self.i2c)
         self.color = self.sensor.color
+        self.get_color_from_sensor()
+        self.get_color_rgb_from_sensor()
 
     # returns rgb as a hex value
-    def get_color(self):
-        return self.sensor.color
+    def get_color_from_sensor(self):
+        self.color = self.sensor.color
 
     # returns (r, g, b) as a tuple
-    def get_color_rgb(self):
-        return self.sensor.color_rgb_bytes
+    def get_color_rgb_from_sensor(self):
+        self.color_rgb = self.sensor.color_rgb_bytes
 
-    def is_green(self, col):
-        # col = self.get_color_rgb()
+    def get_color_name(self, new_data_from_sensor=True):
+        if new_data_from_sensor:
+            self.get_color_from_sensor(self)
+            self.get_color_rgb_from_sensor(self)
+        if c.is_red(self.color_rgb):
+            print("red")
+        elif c.is_green(self.color_rgb):
+            print("green")
+        elif c.is_white(self.color_rgb):
+            print("white")
+        elif c.is_blue(self.color_rgb):
+            print("blue")
+        else:
+            self.show_color()
+
+    @staticmethod
+    def is_green(col):
         if col[0] < 20 and 20 < col[1] and col[2] < 20:
             return True
         return False
 
-    def is_red(self, col):
-        # col = self.get_color_rgb()
+    @staticmethod
+    def is_red(col):
         if 20 < col[0] and col[1] < 20 and col[2] < 20:
             return True
         return False
 
-    def is_blue(self, col):
-        # col = self.get_color_rgb()
+    @staticmethod
+    def is_blue(col):
         if col[0] < 20 and col[1] < 20 and 20 < col[2]:
             return True
         return False
 
-    def is_white(self, col):
-        # col = self.get_color_rgb()
+    @staticmethod
+    def is_white(col):
         if col[0] < 20 and col[1] < 20 and col[2] < 20:
             return True
         return False
 
     def show_color(self):
-        print("RGB color as 8 bits per channel int: #{0:02X} or as a 3-tuple: {1}".format(self.get_color(),
-                                                                                          self.get_color_rgb()))
+        print("RGB color as 8 bits per channel int:"+
+              " #{0:02X} or as a 3-tuple: {1}".format(self.color,self.color_rgb))
 
 
 if __name__ == "__main__":
     c = colorSens()
     while True:
-        color = c.get_color()
-        color_rgb = c.get_color_rgb()
-        if c.is_red(color_rgb):
-            print("red")
-        elif c.is_green(color_rgb):
-            print("green")
-        elif c.is_white(color_rgb):
-            print("white")
-        elif c.is_blue(color_rgb):
-            print("blue")
-        else:
-            print("RGB color as 8 bits per channel int: " +
-                  "#{0:02X} or as a 3-tuple: {1}".format(color, color_rgb))
+        c.get_color_name()
 
