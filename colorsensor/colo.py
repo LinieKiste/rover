e886637d2a7f59c7e363662f1e4c0cb39f721823
Leander <leander.buerkin@gmail.com>
2021-10-18 16:29:56 +0000

    detection for colors white, green and blue added

--- colorsensor/colo.py
+++ colorsensor/colo.py
@@ -4,8 +4,9 @@ import board
 import adafruit_tcs34725
 import time
 
+
 class colorSens:
-    
+
     def __init__(self):
         self.i2c = board.I2C()
         self.sensor = adafruit_tcs34725.TCS34725(self.i2c)
@@ -19,14 +20,33 @@ class colorSens:
     def get_color_rgb(self):
         return self.sensor.color_rgb_bytes
 
+    def is_green(self):
+        col = self.get_color_rgb()
+        if 10 < col[0] < 20 and 30 < col[1] < 40 and 10 < col[2] < 20:
+            return True
+        return False
+
     def is_red(self):
         col = self.get_color_rgb()
-        if col[0] > 30 and col[1] < 15 and col[2] < 15:
+        if 40 < col[0] < 60 and 0 < col[1] < 15 and 0 < col[2] < 15:
+            return True
+        return False
+
+    def is_blue(self):
+        col = self.get_color_rgb()
+        if 0 < col[0] < 10 and 10 < col[1] < 20 and 25 < col[2] < 40:
+            return True
+        return False
+
+    def is_white(self):
+        col = self.get_color_rgb()
+        if 10 < col[0] < 20 and 10 < col[1] < 20 and 10 < col[2] < 20:
             return True
         return False
 
     def show_color(self):
-        print("RGB color as 8 bits per channel int: #{0:02X} or as a 3-tuple: {1}".format(self.get_color(), self.get_color_rgb()))
+        print("RGB color as 8 bits per channel int: #{0:02X} or as a 3-tuple: {1}".format(self.get_color(),
+                                                                                          self.get_color_rgb()))
 
 
 if __name__ == "__main__":
@@ -38,9 +58,8 @@ if __name__ == "__main__":
         color_rgb = c.get_color_rgb()
 
         if prev_color != color:
-            print("RGB color as 8 bits per channel int: #{0:02X} or as a 3-tuple: {1}".format(
-                color, color_rgb
-                )
-            )
-        
-        
+            if not (c.is_red()) and not (c.is_blue()) and \
+                    not (c.is_white()) and not (c.is_green()):
+                print("RGB color as 8 bits per channel int: " +
+                      "#{0:02X} or as a 3-tuple: {1}".format(
+                          color, color_rgb))
--- navigation.py
+++ navigation.py
@@ -6,30 +6,12 @@ from distanceSensor import rpTut
 import explorerhat
 import time
 
-counter = 0
-
-def findline(m):
-    m.stop()
-    if counter < 9:
-        m.rotate(0.1)
-        time.sleep(1)
-    else:
-        m.rotate(-40)
-        time.sleep(1)
 
 def nav(m, c):
     while True:
-        if rpTut.distance() > 10:
-            if c.is_red():
-                counter = 0
-                explorerhat.motor.forward(80)
-            else:
-                m.stop()
-                explorerhat.motor.one.forward(0)
-                explorerhat.motor.two.forward(80)
-                # findline(m)
-        else:
-            m.stop()
+        while c.is_red():
+            explorerhat.motor.one.forward(0)
+            explorerhat.motor.two.forward(80)
 
 
 if __name__ == "__main__":
