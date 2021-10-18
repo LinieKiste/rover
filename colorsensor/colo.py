4a44fde6b794796772aaeb472c15515d384045fa
Leander <leander.buerkin@gmail.com>
2021-10-18 17:56:27 +0000

    added program which can follow a green line between a red and a blue line in theory

--- colorsensor/colo.py
+++ colorsensor/colo.py
@@ -2,10 +2,9 @@
 
 import board
 import adafruit_tcs34725
-import time
 
 
-class colorSens:
+class ColorSensor:
 
     color = None
     color_rgb = None
@@ -25,37 +24,39 @@ class colorSens:
     def get_color_rgb_from_sensor(self):
         self.color_rgb = self.sensor.color_rgb_bytes
 
-    def get_color_name(self, new_data_from_sensor=True):
-        print(self.color_rgb)
-        if new_data_from_sensor:
+    def get_color_name(self, get_new_data_from_sensor=True):
+        # print(self.color_rgb)
+        if get_new_data_from_sensor:
             self.get_color_from_sensor()
             self.get_color_rgb_from_sensor()
-        if c.is_red(self.color_rgb):
-            print("red")
-        elif c.is_green(self.color_rgb):
-            print("green")
-        elif c.is_white(self.color_rgb):
-            print("white")
-        elif c.is_blue(self.color_rgb):
-            print("blue")
+
+        if self.is_red():
+            return "red"
+        elif self.is_green():
+            return "green"
+        elif self.is_white():
+            return "white"
+        elif self.is_blue():
+            return "blue"
+        elif self.is_between_green_and_blue():
+            return "between_green_and_blue"
         else:
             self.show_color()
 
-    @staticmethod
-    def is_green(col):
-        return 20 <= col[1]
+    def is_between_green_and_blue(self):
+        return self.color_rgb[0] < 10 and 10 <= self.color_rgb[1] and 10 <= self.color_rgb[2]
 
-    @staticmethod
-    def is_red(col):
-        return 20 <= col[0] and col[1] < 20 and col[2] < 20
+    def is_green(self):
+        return self.color_rgb[0] < 20 and 20 <= self.color_rgb[1] and self.color_rgb[2] < 20
 
-    @staticmethod
-    def is_blue(col):
-        return col[0] < 20 and col[1] < 20 and 20 <= col[2]
+    def is_red(self):
+        return 20 <= self.color_rgb[0] and self.color_rgb[1] < 20 and self.color_rgb[2] < 20
 
-    @staticmethod
-    def is_white(col):
-        return col[0] < 20 and col[1] < 20 and col[2] < 20
+    def is_blue(self):
+        return self.color_rgb[0] < 20 and self.color_rgb[1] < 20 and 20 <= self.color_rgb[2]
+
+    def is_white(self):
+        return self.color_rgb[0] < 20 and self.color_rgb[1] < 20 and self.color_rgb[2] < 20
 
     def show_color(self):
         print("RGB color as 8 bits per channel int:" +
@@ -63,7 +64,6 @@ class colorSens:
 
 
 if __name__ == "__main__":
-    c = colorSens()
+    color_sensor = ColorSensor()
     while True:
-        c.get_color_name()
-
+        print(color_sensor.get_color_name())
--- navigation.py
+++ navigation.py
@@ -7,15 +7,27 @@ import explorerhat
 import time
 
 
-def nav(m, c):
+def nav(color_sensor):
+    start = time.time()
     while True:
-        while c.is_red():
-            explorerhat.motor.one.forward(0)
+        while color_sensor.get_color_name() == "green":
+            explorerhat.motor.one.forward(80)
             explorerhat.motor.two.forward(80)
+        while color_sensor.get_color_name() == "red":
+            explorerhat.motor.one.forward(80)
+            explorerhat.motor.two.forward(-80)
+        while color_sensor.get_color_name() == "blue":
+            explorerhat.motor.one.forward(-80)
+            explorerhat.motor.two.forward(80)
+        counter = 0
+        while color_sensor.get_color_name() == "white" and counter < 100:
+            explorerhat.motor.one.forward(80)
+            explorerhat.motor.two.forward(80)
+        while color_sensor.get_color_name() == "white":
+            explorerhat.motor.one.forward(0)
+            explorerhat.motor.two.forward(0)
 
 
 if __name__ == "__main__":
-    m = motors.motors(50)
-    c = colo.colorSens()
-    nav(m, c)
-    # m.keyboard_unbuffered()
+    color_sensor = colo.ColorSensor()
+    nav(color_sensor)
