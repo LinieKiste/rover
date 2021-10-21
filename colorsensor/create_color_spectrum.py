658d2722460f4abedefcb9d4975e1fd857cc5bb5
Leander <leander.buerkin@gmail.com>
2021-10-21 15:57:44 +0000

    added more samples to the color_spectrum detection

--- colorsensor/create_color_spectrum.py
+++ colorsensor/create_color_spectrum.py
@@ -7,13 +7,12 @@ import time
 
 
 def create_color_spectrum(color_sensor):
-    for _ in range(10000):
+    for _ in range(200000):
         color_sensor.get_color_from_sensor()
         f = open("color_spectrum.txt", "a")
-        f.write(str(color_sensor.get_color_rgb()) + ",\n")
-        f.write(",")
+        f.write(color_sensor.get_color_rgb())
         f.close()
-        time.sleep(0.01)
+        time.sleep(0.001)
 
 
 if __name__ == "__main__":
