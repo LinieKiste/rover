d790ba304db05904cdafe1e8017196ffaf1d38db
Leander <leander.buerkin@gmail.com>
2021-10-22 15:48:43 +0000

    pid controlled single line following

--- navigation_leander.py
+++ navigation_leander.py
@@ -13,16 +13,16 @@ import simple_pid
 def nav(color_sensor):
     explorerhat.motor.forward(100)
     time.sleep(0.01)
-    pid_controller = simple_pid.PID(20, 4, 2.5, setpoint=0)
+    pid_controller = simple_pid.PID(10, 0, 0, setpoint=0)
     while True:
         color_sensor.get_color_from_sensor()
         red_value = color_sensor.get_color_rgb()[0]
         control = pid_controller(red_value/120 - 0.6)
         print(control)
         base_throttle = 50
-        offset = -4.5
-        left = base_throttle + offset + control
-        right = base_throttle - offset - control
+        offset = 4.5
+        left = base_throttle - offset - control
+        right = base_throttle + offset + control
 
         if left >= 0:
             explorerhat.motor.one.forward(left)
