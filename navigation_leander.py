cb3f30d277270d306085a74084c95cb7d951a479
rover <cwallwitz@gmail.com>
2021-10-22 15:05:26 +0000

    can you push the code???

--- navigation_leander.py
+++ navigation_leander.py
@@ -13,14 +13,14 @@ import simple_pid
 def nav(color_sensor):
     explorerhat.motor.forward(100)
     time.sleep(0.01)
+    pid_controller = simple_pid.PID(20, 4, 2.5, setpoint=0)
     while True:
         color_sensor.get_color_from_sensor()
         red_value = color_sensor.get_color_rgb()[0]
         control = pid_controller(red_value/120 - 0.6)
         print(control)
-        control = 0
         base_throttle = 50
-        offset = 4
+        offset = -4.5
         left = base_throttle + offset + control
         right = base_throttle - offset - control
 
