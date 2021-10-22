355d441d6cf089f57c6ba7a9d2a2542d3d7a956f
Leander <leander.buerkin@gmail.com>
2021-10-22 14:36:41 +0000

    pid controlled single line following

--- navigation_leander.py
+++ navigation_leander.py
@@ -6,12 +6,33 @@ from distanceSensor import rpTut
 import explorerhat
 from explorerhat import motor
 import time
+import simple_pid
 
 
 
 def nav(color_sensor):
+    explorerhat.motor.forward(100)
+    time.sleep(0.01)
     while True:
-        collect_colors()
+        color_sensor.get_color_from_sensor()
+        red_value = color_sensor.get_color_rgb()[0]
+        control = pid_controller(red_value/120 - 0.6)
+        print(control)
+        control = 0
+        base_throttle = 50
+        offset = 4
+        left = base_throttle + offset + control
+        right = base_throttle - offset - control
+
+        if left >= 0:
+            explorerhat.motor.one.forward(left)
+        else:
+            explorerhat.motor.one.backward(abs(left))
+        if right >= 0:
+            explorerhat.motor.two.forward(right)
+        else:
+            explorerhat.motor.two.backward(abs(right))
+
 
 if __name__ == "__main__":
     color_sensor1 = colo.ColorSensor()
