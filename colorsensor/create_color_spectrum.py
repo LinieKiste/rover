bcb7401639665673252ba92f98a6a479455c62f6
Leander <leander.buerkin@gmail.com>
2021-10-22 13:14:06 +0000

    automatic color spectrum detection

--- colorsensor/create_color_spectrum.py
+++ colorsensor/create_color_spectrum.py
@@ -7,22 +7,21 @@ import time
 
 
 def create_color_spectrum(color_sensor):
-    motor.forward(100)
-    time.sleep(0.03)
-    motor.forward(40)
-    time.sleep(5)
-    motor.stop()
-    return
-    for _ in range(20000):
-        f = open("color_spectrum_red.txt", "a")
+    for i in range(5):
+        motor.forward(100)
+        time.sleep(0.1)
+        motor.forward(50)
+        f = open("color_spectrum_red_"+str(i)+".txt", "w")
         color_sensor.get_color_from_sensor()
-        f.write(str(color_sensor.get_color_rgb())+", ")
-        time.sleep(0.001)
+        f.write(str(color_sensor.get_color_rgb()))
         f.close()
-    f = open("color_spectrum_red.txt", "a")
-    color_sensor.get_color_from_sensor()
-    f.write(str(color_sensor.get_color_rgb()))
-    f.close()
+        for _ in range(100000):
+            f = open("color_spectrum_red_"+str(i)+".txt", "a")
+            color_sensor.get_color_from_sensor()
+            f.write(", " + str(color_sensor.get_color_rgb()))
+            f.close()
+        motor.stop()
+        time.sleep(5)
 
 
 if __name__ == "__main__":
